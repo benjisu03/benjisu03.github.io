cde1a3db2cb66e2ccd6ae930babf971ad6e3124d
benjisu <benj2003@iastate.edu>
2025-04-24 14:00:09 -0500

    project card pictures

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,18 +7,22 @@ export enum CardCorner {
 
 type ProjectCardProps = {
 	corner: CardCorner;
+	name: string;
 	className?: string;
 }
 
-const ProjectCard = ({ corner, className = "" }: ProjectCardProps) => {
+const ProjectCard = ({ corner, name, className = "" }: ProjectCardProps) => {
 	return (
 		<div className={`
             ${className}
-            p-[30px]
+            relative
+            p-[15px]
             bg-background
             border border-border ${corner}
             shadow-xl
         `}>
+			<div className = {`absolute inset-0 bg-[linear-gradient(to_bottom,_rgba(0,0,0,0.5)_0%,_rgba(0,0,0,0)_50%)] ${corner}`}></div>
+			<div className="relative z-10 text-white font-semibold">{name}</div>
 		</div>
 	);
 }
--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -7,10 +7,10 @@ type ProjectsProps = {
 const Projects = ({ className = "" }: ProjectsProps) => {
 	return (
 		<div className = { `${className} grid grid-cols-2` }>
-			<ProjectCard corner = {CardCorner.TopLeft} />
-			<ProjectCard corner = {CardCorner.TopRight} />
-			<ProjectCard corner = {CardCorner.BottomLeft} />
-			<ProjectCard corner = {CardCorner.BottomRight} />
+			<ProjectCard corner = {CardCorner.TopLeft} name = "Grayshift" className = {"bg-[url('/images/grayshift.png')] bg-cover bg-center h-64"} />
+			<ProjectCard corner = {CardCorner.TopRight} name = "Valley West - Renovation" className = {"bg-[url('/images/vwm-edit.png')] bg-cover bg-center h-64"} />
+			<ProjectCard corner = {CardCorner.BottomLeft} name = "Non-Euclidean Rendering Engine" className = {"bg-[url('/images/non-euclid-edit.png')] bg-cover bg-center h-64"} />
+			<ProjectCard corner = {CardCorner.BottomRight} name = "USB-C Power Supply" className = {"bg-[url('/images/PCB-render.png')] bg-cover bg-center h-64"} />
 		</div>
 	);
 }
