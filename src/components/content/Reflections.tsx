2c5c39245ff80172950e5353398b3fcf9f2e8c7a
benjisu <benj2003@iastate.edu>
2025-05-08 21:32:36 -0500

    PDF reflections

--- src/components/content/Reflections.tsx
+++ src/components/content/Reflections.tsx
@@ -1,6 +1,3 @@
-import EthicsEssay from "@/components/essays/EthicsEssay";
-import GeneralEducationReflection from "@/components/essays/GeneralEducationReflection";
-import CumulativeReflection from "@/components/essays/CumulativeReflection";
 
 type ReflectionsProps = {
 	className?: string
@@ -10,13 +7,29 @@ const Reflections = ({ className = "" }: ReflectionsProps) => {
 	return (
 		<div className = { `${className}` }>
 			<h1 className = { "text-xl font-semibold mt-[30px] mb-[10px]" }>General Education Reflection</h1>
-			<p className = { "text-sm text-justify" }> { GeneralEducationReflection } </p>
+			<iframe
+				src="/reflections/GeneralEducationReflection.pdf"
+				width="100%"
+				height="600px"
+				style={{ border: "none" }}
+			/>
 
 			<h1 className = { "text-xl font-semibold mt-[30px] mb-[10px]" }>Cumulative Reflection</h1>
-			<p className = { "text-sm text-justify" }> { CumulativeReflection } </p>
+			<iframe
+				src="/reflections/CumulativeReflection.pdf"
+				width="100%"
+				height="600px"
+				style={{ border: "none" }}
+			/>
 
 			<h1 className = { "text-xl font-semibold mt-[30px] mb-[10px]" }>Ethics Essay</h1>
-			<p className = { "text-sm text-justify whitespace-pre-line" }> { EthicsEssay } </p>
+			<iframe
+				src="/reflections/EthicsEssay.pdf"
+				width="100%"
+				height="600px"
+				style={{ border: "none" }}
+			/>
+
 		</div>
 	);
 }
