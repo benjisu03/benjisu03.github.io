90680d87418c1bc3bf358bdb4b31196ede049d7c
Benjamin Johnson <benj2003@iastate.edu>
2025-08-13 00:25:08 -0500

    fixed

--- src/components/Separator.tsx
+++
@@ -1,9 +0,0 @@
-type SeparatorProps = {
-    className?: string
-}
-
-const Separator = ({className = "" }: SeparatorProps) => {
-    return <div className={`w-full h-[1px] bg-border ${className}`}></div>
-}
-
-export default Separator;
--- src/components/TitleBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function TitleBar() {
-    return (
-        <div className="w-full p-4 bg-background text-foreground text-center text-xl font-bold">
-            BENJAMIN JOHNSON
-        </div>
-    );
-}
